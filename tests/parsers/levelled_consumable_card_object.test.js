c29d41cc1f209b91e59f056fdbe05234834c83f0
richseviora <richard.seviora@gmail.com>
2017-04-21 14:50:05 -0700

    Added Tests for All Levels

--- tests/parsers/levelled_consumable_card_object.test.js
+++ tests/parsers/levelled_consumable_card_object.test.js
@@ -3,7 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var levelled_consumable_card_object_1 = require("../../parsers/levelled_consumable_card_object");
 var Cheerio = require("cheerio");
 describe('LevelledConsumableCardObject', function () {
-    var sampleCard = '<div class="card" style="background: url(\'//lvlt.bioware.cdn.ea.com/bioware/u/f/eagames/bioware/masseffect3/n7hq/game2webaxis/images/masseffect3/icons/multiplayer/cards/common-on.png\') no-repeat 50% 0;"><img src="//lvlt.bioware.cdn.ea.com/bioware/u/f/eagames/bioware/masseffect3/n7hq/game2webaxis/images/masseffect3/icons/multiplayer/consumables/WeaponDamageBonus_AssaultRifle.png"><span>Assault Rifle Rail Amp I - 4</span><span>Assault Rifle Rail Amp II - 1</span><span>Assault Rifle Rail Amp III - 2</span></div>';
+    var sampleCard = '<div class="card" style="background: url(\'//lvlt.bioware.cdn.ea.com/bioware/u/f/eagames/bioware/masseffect3/n7hq/game2webaxis/images/masseffect3/icons/multiplayer/cards/common-on.png\') no-repeat 50% 0;"><img src="//lvlt.bioware.cdn.ea.com/bioware/u/f/eagames/bioware/masseffect3/n7hq/game2webaxis/images/masseffect3/icons/multiplayer/consumables/WeaponDamageBonus_AssaultRifle.png"><span>Assault Rifle Rail Amp I - 4</span><span>Assault Rifle Rail Amp II - 1</span><span>Assault Rifle Rail Amp III - 0</span></div>';
     var co;
     beforeAll(function () {
         var card = Cheerio(sampleCard);
@@ -14,6 +14,12 @@ describe('LevelledConsumableCardObject', function () {
     });
     it('returns the correct level 1 entry', function () {
         expect(co.entries).toContainEqual({ name: 'Assault Rifle Rail Amp', level: 1, quantity: 4 });
+    });
+    it('returns the correct level 2 entry', function () {
+        expect(co.entries).toContainEqual({ name: 'Assault Rifle Rail Amp', level: 2, quantity: 1 });
+    });
+    it('returns the correct level 3 entry', function () {
+        expect(co.entries).toContainEqual({ name: 'Assault Rifle Rail Amp', level: 3, quantity: 0 });
     });
 });
 //# sourceMappingURL=levelled_consumable_card_object.test.js.map
--- tests/parsers/levelled_consumable_card_object.test.ts
+++ tests/parsers/levelled_consumable_card_object.test.ts
@@ -1,20 +1,26 @@
-import {LevelledConsumableCardObject} from '../../parsers/levelled_consumable_card_object';
+import { LevelledConsumableCardObject } from '../../parsers/levelled_consumable_card_object';
 import * as Cheerio from 'cheerio';
 
 
 describe('LevelledConsumableCardObject', () => {
 
-    let sampleCard = '<div class="card" style="background: url(\'//lvlt.bioware.cdn.ea.com/bioware/u/f/eagames/bioware/masseffect3/n7hq/game2webaxis/images/masseffect3/icons/multiplayer/cards/common-on.png\') no-repeat 50% 0;"><img src="//lvlt.bioware.cdn.ea.com/bioware/u/f/eagames/bioware/masseffect3/n7hq/game2webaxis/images/masseffect3/icons/multiplayer/consumables/WeaponDamageBonus_AssaultRifle.png"><span>Assault Rifle Rail Amp I - 4</span><span>Assault Rifle Rail Amp II - 1</span><span>Assault Rifle Rail Amp III - 2</span></div>'
+    let sampleCard = '<div class="card" style="background: url(\'//lvlt.bioware.cdn.ea.com/bioware/u/f/eagames/bioware/masseffect3/n7hq/game2webaxis/images/masseffect3/icons/multiplayer/cards/common-on.png\') no-repeat 50% 0;"><img src="//lvlt.bioware.cdn.ea.com/bioware/u/f/eagames/bioware/masseffect3/n7hq/game2webaxis/images/masseffect3/icons/multiplayer/consumables/WeaponDamageBonus_AssaultRifle.png"><span>Assault Rifle Rail Amp I - 4</span><span>Assault Rifle Rail Amp II - 1</span><span>Assault Rifle Rail Amp III - 0</span></div>'
 
     let co: LevelledConsumableCardObject;
     beforeAll(() => {
         const card = Cheerio(sampleCard);
         co = new LevelledConsumableCardObject(card);
     });
-    it('returns the correct name',()=> {
+    it('returns the correct name', () => {
         expect(co.humanName).toEqual('Assault Rifle Rail Amp');
     });
-    it('returns the correct level 1 entry',() => {
-        expect(co.entries).toContainEqual({name: 'Assault Rifle Rail Amp', level: 1, quantity: 4})
+    it('returns the correct level 1 entry', () => {
+        expect(co.entries).toContainEqual({ name: 'Assault Rifle Rail Amp', level: 1, quantity: 4 })
+    })
+    it('returns the correct level 2 entry', () => {
+        expect(co.entries).toContainEqual({ name: 'Assault Rifle Rail Amp', level: 2, quantity: 1 })
+    })
+    it('returns the correct level 3 entry', () => {
+        expect(co.entries).toContainEqual({ name: 'Assault Rifle Rail Amp', level: 3, quantity: 0 })
     })
 });
